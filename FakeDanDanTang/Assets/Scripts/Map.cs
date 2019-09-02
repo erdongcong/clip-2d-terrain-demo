36e224f1e38b9a65b62c920a9a3912c4a0ea92fe
erdongcong <chen_jiajun_xb@qq.com>
2019-09-02 18:00:58 +0800

    demo
    an idea of destroy 2d terrain

---
+++ FakeDanDanTang/Assets/Scripts/Map.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using ClipperLib;
+
+using Path = System.Collections.Generic.List<ClipperLib.IntPoint>;
+using Paths = System.Collections.Generic.List<System.Collections.Generic.List<ClipperLib.IntPoint>>;
+
+public class Map : MonoBehaviour
+{
+    public GameObject maskPrefab;
+
+    private PolygonCollider2D m_Collider2D;
+    private Paths m_ClipPaths;
+    private Paths m_ColliderPaths;
+
+    private float m_ColliderPointScale = 1000.0f;
+
+
+    private void Awake()
+    {
+        // Setting up references.
+        m_Collider2D = GetComponent<PolygonCollider2D>();
+
+        m_ClipPaths = new Paths();
+        m_ClipPaths.Add(new Path());
+
+        m_ColliderPaths = new Paths();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 collideWorldPos = collision.GetContact(0).point;
+        Vector2 collidePoint = transform.InverseTransformPoint(collideWorldPos);
+
+        m_ClipPaths[0].Clear();
+        int x = (int)((collidePoint.x - 0.5f) * m_ColliderPointScale);
+        int y = (int)((collidePoint.y + 0.5f) * m_ColliderPointScale);
+        m_ClipPaths[0].Add(new IntPoint(x, y));
+        x = (int)((collidePoint.x + 0.5f) * m_ColliderPointScale);
+        y = (int)((collidePoint.y + 0.5f) * m_ColliderPointScale);
+        m_ClipPaths[0].Add(new IntPoint(x, y));
+        x = (int)((collidePoint.x + 0.5f) * m_ColliderPointScale);
+        y = (int)((collidePoint.y - 0.5f) * m_ColliderPointScale);
+        m_ClipPaths[0].Add(new IntPoint(x, y));
+        x = (int)((collidePoint.x - 0.5f) * m_ColliderPointScale);
+        y = (int)((collidePoint.y - 0.5f) * m_ColliderPointScale);
+        m_ClipPaths[0].Add(new IntPoint(x, y));
+
+        for(int i = m_ColliderPaths.Count; i < m_Collider2D.pathCount; i++)
+        {
+            m_ColliderPaths.Add(new Path());
+        }
+
+        for(int j = 0; j < m_ColliderPaths.Count; j++)
+        {
+            m_ColliderPaths[j].Clear();
+        }
+
+        for(int k = 0; k < m_Collider2D.pathCount; k++)
+        {
+            Vector2[] path = m_Collider2D.GetPath(k);
+            Path clippedPath = m_ColliderPaths[k];
+            for(int m = 0; m < path.Length; m++)
+            {
+                x = (int)(path[m].x * m_ColliderPointScale);
+                y = (int)(path[m].y * m_ColliderPointScale);
+                clippedPath.Add(new IntPoint(x, y));
+            }
+        }
+
+        Paths result = new Paths();
+        Clipper c = new Clipper();
+        c.AddPaths(m_ColliderPaths, PolyType.ptSubject, true);
+        c.AddPaths(m_ClipPaths, PolyType.ptClip, true);
+        c.Execute(ClipType.ctDifference, result, PolyFillType.pftEvenOdd, PolyFillType.pftNonZero);
+
+        m_Collider2D.pathCount = result.Count;
+        for(int n = 0; n < result.Count; n++)
+        {
+            Path resultPath = result[n];
+            List<Vector2> colliderPath = new List<Vector2>();
+            for (int l = 0; l < resultPath.Count; l++)
+            {
+                colliderPath.Add(new Vector2((float)resultPath[l].X / m_ColliderPointScale, (float)resultPath[l].Y / m_ColliderPointScale));
+                //Debug.Log("新网格：( " + resultPath[l].X + ", " + resultPath[l].Y + " )");
+            }
+            m_Collider2D.SetPath(n, colliderPath.ToArray());
+        }
+
+        Instantiate(maskPrefab, new Vector3(collideWorldPos.x, collideWorldPos.y, 0.0f), Quaternion.identity);
+    }
+}
---
+++ FakeDanDanTang/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+    [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
+
+
+    private Rigidbody2D m_Rigidbody2D;
+
+
+    private void Awake()
+    {
+        // Setting up references.
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(float move, bool jump)
+    {
+        // Move the character
+        m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+
+        // If jump
+        if(jump)
+        {
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
+    }
+}
